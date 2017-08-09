2dd9813cb03ecc42e4c29e210537ca9825d32dcc
sridhar1910 <nani6006@gmail.com>
2017-08-09 14:07:28 -0500

    Initial commit

---
+++ src/com/factory/Main.java
@@ -0,0 +1,11 @@
+package com.factory;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //int i=0;
+	for(int i=0;i<=100;i++){
+	    System.out.println(i);
+    }
+    }
+}
