3d1071ce2985f21821ba1561a2218da546b1ac18
sridhar1910 <nani6006@gmail.com>
2017-08-09 16:12:26 -0500

    even commit

--- src/com/factory/Main.java
+++ src/com/factory/Main.java
@@ -5,7 +5,13 @@ public class Main {
     public static void main(String[] args) {
         //int i=0;
 	for(int i=0;i<=100;i++){
-	    System.out.println(i);
+	    System.out.print(i);
+    }
+    System.out.println("Printing Even Numbers");
+    for(int i=0;i<=100;i++){
+	    if(i%2 ==0 && i!=2){
+	        System.out.print(i+" ");
+        }
     }
     }
 }
