d0862c08f6ddf2f952165f86baaba294c516734d
Anoohya Makkapati <anoohyamakkapati@gmail.com>
2017-08-09 15:07:29 -0500

    printing prime numbers btw 1 to 10

--- src/com/factory/Main.java
+++ src/com/factory/Main.java
@@ -4,8 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         //int i=0;
-	for(int i=0;i<=100;i++){
-	    System.out.println(i);
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Printing Prime Numbers");
+        for (int i = 0; i <= 100; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+                if(isPrime){
+                    System.out.print(i+" ");
+                }
+
+            }
+        }
+
     }
-    }
-}
